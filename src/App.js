860776c5097a2881987a80450f6fda510f9dce1a
Packooo <you@example.com>
2023-10-02 21:04:24 +0700

    Penambahan fungsi file Expense Item

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Expenseitem from "./components/ExpenseItem";
+
 function App() {
   return ( 
     <div>
       <h2> Let 's get started!</h2>  
-      <p> This is also possible! </p>  
+      <Expenseitem></Expenseitem>  
     </div>
   );
 }
